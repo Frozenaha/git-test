98419930dff4b21ff727cc2fc92542b152870f26
wwz <1255340634@qq.com>
2023-08-17 22:56:27 +0800

    dayjs

--- src/pages/docs.tsx
+++ src/pages/docs.tsx
@@ -1,4 +1,11 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(utc);
 const DocsPage = () => {
+  const a = dayjs();
+  console.log(a.utc(true).format());
+  console.log(a.format());
   return (
     <div>
       <p>This is umi dos.</p>
